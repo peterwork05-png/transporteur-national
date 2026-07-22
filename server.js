037e0eff2037782f3835f254930112a929a31d70
peterwork05-png <peter.work05@gmail.com>
2026-07-22 02:45:56 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.json());
-
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 // API routes
 app.use('/api', apiRoutes);
 
